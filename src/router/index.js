d1157c09b0de259306b62e6902e0b79e54ca66ed
Terry.D (office pc) <softwaltz@gmail.com>
2024-08-22 13:36:00 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/signup',
+      component: () => import('../views/SignupView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
